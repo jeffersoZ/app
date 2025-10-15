40f7750afd43e784c59503c82869cbeff37ce917
jeffersoZ <jefferson_zimmermann@hotmail.com>
2025-10-15 20:10:02 -0300

    launcher

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,24 +1,38 @@
 package com.example.myapplication;
 
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.ListView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    PackageManager pm;
+    ListView listView;
+    List<ApplicationInfo> apps;
+    ArrayList<ApplicationInfo> userApps;
+    AppAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        listView = findViewById(R.id.listView);
+        pm = getPackageManager(); // Recupera gerenciador de pacotes
+        apps = pm.getInstalledApplications(PackageManager.GET_META_DATA);
+        userApps = new ArrayList<>();
+
+        for(ApplicationInfo app : apps) {
+            if((app.flags & ApplicationInfo.FLAG_SYSTEM) == 0) {
+                userApps.add(app);
+            };
+        }
+
+        adapter = new AppAdapter(this, apps, pm);
+        listView.setAdapter(adapter);
     }
 }
